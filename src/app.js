6c0c8f8996ebe3540d007783ab2384a72e882d9a
johngorithm <johngorithm@gmail.com>
2018-06-26 08:39:16 +0100

    task: Add static js, html and css files for index page usage
    This is to allow heroku display a page for the index end-point on landing
    rather than CAN'T GET \ error message

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // static
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/../public`));
 
 // routes
 app.use('/', indexRoute);
