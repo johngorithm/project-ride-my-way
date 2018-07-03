02ae106daf778b15edf886117dd0c9a3347bac09
johngorithm <johngorithm@gmail.com>
2018-07-03 12:39:49 +0100

    TASK<AUTHORIZATION>: LOGIN A REGISTERED USER INTO THE APP
    This allow a registered user have access to the app while unregistered
    users are notified of their status and offered suggestion to do so
    
    FINISHED ##15857609 #158576090

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -67,5 +67,64 @@ authRouter.post('/signup', (req, res) => {
   }
 });
 
+authRouter.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const fieldErrors = {};
+  let isDataValid = true;
+
+  const validateUser = (fieldData, fieldName) => {
+    if (typeof fieldData === 'string') {
+      if (fieldData.trim() === '') {
+        fieldErrors[fieldName] = `${fieldName} is required`;
+        isDataValid = false;
+      }
+    } else if (fieldData === undefined) {
+      fieldErrors[fieldName] = `${fieldName} is required`;
+      isDataValid = false;
+    }
+  };
+
+  validateUser(username, 'username');
+  validateUser(password, 'password');
+
+  if (isDataValid) {
+    pool.query('SELECT * FROM users WHERE username = $1', [username], (error, user) => {
+      if (error) {
+        res.status(500).json({
+          message: 'Something went wrong, user data retrieval was not successful!',
+          status: false,
+          user: req.body,
+          error: error.message,
+        });
+      } else if (user.rows[0]) {
+        if (bcrypt.compareSync(password, user.rows[0].password)) {
+          res.status(200).json({
+            message: `Welcome ${user.rows[0].firstname}, you are successfully logged in`,
+            status: true,
+            user: user.rows[0],
+          });
+        } else {
+          res.status(200).json({
+            message: `${user.rows[0].firstname}, your password is incorrect, please check and try again`,
+            status: false,
+            user: req.body,
+          });
+        }
+      } else {
+        res.status(404).json({
+          message: `${username} does not exist, please check and try again`,
+          status: false,
+          error: 'User Not Found!',
+        });
+      }
+    });
+  } else {
+    res.status(400).json({
+      message: 'The data you entered is invalid',
+      status: false,
+      error: 'Invalid Data Submitted!',
+    });
+  }
+});
 
 export default authRouter;
